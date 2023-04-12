b400b05ef2abe44929aa2be6df55647c5aa158d8
jazzcodes <jasleenbhambra01@gmail.com>
2023-04-12 17:56:16 +0530

    5 Projects done

---
+++ Projects/Project-5/app.js
@@ -0,0 +1,48 @@
+const addTaskBtn=document.getElementById('add');
+const taskContainer=document.getElementById('container');
+const inputTask=document.getElementById('todo-input');
+
+addTaskBtn.addEventListener("click", function()
+
+{
+    let task=document.createElement('div');
+    task.classList.add('task');
+
+    let li=document.createElement('li');
+    li.innerText=`${inputTask.value}`;
+    task.appendChild(li);
+    console.log("clicked");
+    console.log(li.innerText);
+
+
+    if(inputTask.value==="")
+    {
+        alert("Please enter a task");
+    }
+    else{
+        taskContainer.appendChild(task);
+    }
+
+    let checkButton=document.createElement('button');
+    checkButton.innerHTML=`<i class="fa fa-check"></i>`;
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deleteButton=document.createElement('button');
+    deleteButton.innerHTML=`<i class="fa fa-trash"></i>`;
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    inputTask.value="";
+
+    checkButton.addEventListener("click", function()
+    {
+        checkButton.parentElement.style.textDecoration="line-through";
+    });
+
+    deleteButton.addEventListener("click", function()
+    {
+       deleteButton.parentElement.remove();
+    });
+});
+
